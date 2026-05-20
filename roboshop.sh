be48184ae787d0275f95400cbce7028ac90eae39
SathivilliPraveena836 <veenasathivilli@gmail.com>
2026-05-20 10:52:16 +0530

    roboshop project

--- roboshop.sh
+++ roboshop.sh
@@ -2,28 +2,36 @@
 ami_id=ami-0220d79f3f480ecf5
 sg_id=sg-0cdb841294c052660
 sub_id=subnet-08dff5950b3129d08
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 zone_id=Z096607625SAROAU2N8QT
 site_name=roboticgear.shop
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0cdb841294c052660 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $ami_id \
+    --instance-type t3.micro \
+    --security-group-ids $sg_id \
+    --subnet-id $sub_id \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query "Instances[0].InstanceId" \
+    --output text)
+
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+        RECORD_NAME="$instance.$site_name"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$DOMAIN_NAME"
+        RECORD_NAME="$site_name"
     fi
+
     echo "$instance IP address: $IP"
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
+    --hosted-zone-id $zone_id \
     --change-batch '
     {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
+        "Comment": "Creating or Updating a record set"
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
