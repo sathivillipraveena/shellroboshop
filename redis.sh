bc6422c599e359f13f736635191683cb7c1e10d7
SathivilliPraveena836 <veenasathivilli@gmail.com>
2026-05-23 21:22:32 +0530

    roboshop project

--- redis.sh
+++ redis.sh
@@ -32,19 +32,19 @@ VALIDATE(){
         exit 1
     fi
 }
-dnf module disable redis -y &>>LOG_FILE
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "redis module disabled"
 
-dnf module enable redis:7 -y &>>LOG_FILE
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "redis 7 is enabled"
 
-dnf install redis -y &>>LOG_FILE
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "installing redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 
-systemctl enable redis &>>LOG_FILE
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "enabled redis"
 
-systemctl start redis &>>LOG_FILE
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "started redis"
