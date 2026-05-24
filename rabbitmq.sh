2e68e5cea7e4b4ea7a98057cdff39b1497cdea90
SathivilliPraveena836 <veenasathivilli@gmail.com>
2026-05-24 21:30:03 +0530

    roboshop project

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
@@ -6,23 +7,21 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename $0 .sh)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
+    exit 1
 else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
@@ -32,17 +31,22 @@ VALIDATE(){
         exit 1
     fi
 }
-dnf install rabbitmq-server -y &>>$LOG_FILE
-VALIDATE $? "installation of rabbitmq"
-systemctl enable rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "enabling rabbitmq"
-systemctl start rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "starting rabbitmq"
-rabbitmqctl add_user roboshop roboshop123
-VALIDATE $? "adding user"
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-VALIDATE $? "set permissions"
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VALIDATE $? "Installation of RabbitMQ"
+
+systemctl enable rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "Enabling RabbitMQ"
+
+systemctl start rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "Starting RabbitMQ"
+
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+VALIDATE $? "Adding roboshop user"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+VALIDATE $? "Setting permissions for roboshop user"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( END_TIME - START_TIME ))
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
