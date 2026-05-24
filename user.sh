23fb71d72772423e4837f5f888df74594a4546ab
SathivilliPraveena836 <veenasathivilli@gmail.com>
2026-05-24 21:25:02 +0530

    roboshop project

--- user.sh
+++ user.sh
@@ -7,14 +7,13 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename $0 .sh)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# check the user has root priveleges or not
 if [ $USER_ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
@@ -23,7 +22,6 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
@@ -43,13 +41,13 @@ VALIDATE $? "nodejs20 enabled"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "nodejs installed"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "user is created"
 else
-    echo -e "user already exists"
+    echo -e "user already exists" | tee -a $LOG_FILE
 fi
 
 mkdir -p /app
@@ -57,6 +55,7 @@ VALIDATE $? "directory for app is created"
 
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
 VALIDATE $? "downloaded into tmp"
+
 rm -rf /app/*
 cd /app
 unzip /tmp/user.zip &>>$LOG_FILE
@@ -66,13 +65,17 @@ npm install &>>$LOG_FILE
 VALIDATE $? "nodejs built module installed"
 
 cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Copying user service file"
 
 systemctl daemon-reload &>>$LOG_FILE
 VALIDATE $? "daemon reloaded"
 
 systemctl enable user &>>$LOG_FILE
 VALIDATE $? "enabled user"
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+systemctl start user &>>$LOG_FILE
+VALIDATE $? "started user"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( END_TIME - START_TIME ))
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
