e831cfa4ac3305900b591ef7092bca6ad04c843f
SathivilliPraveena836 <veenasathivilli@gmail.com>
2026-05-24 21:26:56 +0530

    roboshop project

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
@@ -22,6 +23,9 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
+echo "Please enter root password to setup"
+read -s MYSQL_ROOT_PASSWORD
+
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
@@ -32,14 +36,19 @@ VALIDATE(){
         exit 1
     fi
 }
+
 dnf install mysql-server -y &>>$LOG_FILE
-VALIDATE $? "installing mysql server"
-systemctl enable mysqld
-VALIDATE $? "enabling mysql"
-systemctl start mysqld  
-VALIDATE $? "starting mysql"
+VALIDATE $? "Installing MySQL server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabling MySQL"
+
+systemctl start mysqld   &>>$LOG_FILE
+VALIDATE $? "Starting MySQL"
+
 mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
 VALIDATE $? "Setting MySQL root password"
+
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
