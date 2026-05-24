a2fd394174c976731fcb76920164cd7fe5e60193
SathivilliPraveena836 <veenasathivilli@gmail.com>
2026-05-24 21:22:41 +0530

    roboshop project

--- shipping.sh
+++ shipping.sh
@@ -7,23 +7,23 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename $0 .sh)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# check the user has root priveleges or not
+# Check root privileges
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
+    exit 1
 else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
+# VALIDATE function: takes exit status and description
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
@@ -34,60 +34,80 @@ VALIDATE(){
     fi
 }
 
-dnf install maven -y
-id roboshop
+# Install Maven
+dnf install maven -y &>>$LOG_FILE
+VALIDATE $? "Installing Maven"
+
+# Create roboshop system user if not exists
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating roboshop system user"
 else
-    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+    echo -e "System user roboshop already exists ... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
-mkdir -p /app 
+# Create app directory
+mkdir -p /app
 VALIDATE $? "Creating app directory"
+
+# Download shipping artifact
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading Catalogue"
+VALIDATE $? "Downloading Shipping artifact"
 
+# Clean and unzip
 rm -rf /app/*
-cd /app 
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzipping catalogue"
+cd /app
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping Shipping artifact"
 
-mvn clean package 
-VALIDATE $? "mvn build tool"
-mv target/shipping-1.0.jar shipping.jar 
+# Build with Maven
+mvn clean package &>>$LOG_FILE
+VALIDATE $? "Maven build"
+
+# Rename jar
+mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
 VALIDATE $? "Moving and renaming Jar file"
 
+# Copy service file
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE $? "Copying shipping service file"
 
+# Reload and enable service
 systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Daemon Realod"
+VALIDATE $? "Daemon reload"
 
-systemctl enable shipping  &>>$LOG_FILE
+systemctl enable shipping &>>$LOG_FILE
 VALIDATE $? "Enabling Shipping"
 
 systemctl start shipping &>>$LOG_FILE
 VALIDATE $? "Starting Shipping"
 
-dnf install mysql -y  &>>$LOG_FILE
-VALIDATE $? "Install MySQL"
+# Install MySQL client
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL client"
 
-mysql -h mysql.daws84s.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+# Load DB data only if not already loaded
+mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+    VALIDATE $? "Loading schema.sql"
+
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
+    VALIDATE $? "Loading app-user.sql"
+
     mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
-    VALIDATE $? "Loading data into MySQL"
+    VALIDATE $? "Loading master-data.sql"
 else
-    echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
+    echo -e "Data already loaded into MySQL ... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
+# Restart shipping after DB load
 systemctl restart shipping &>>$LOG_FILE
-VALIDATE $? "Restart shipping"
+VALIDATE $? "Restarting Shipping"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+TOTAL_TIME=$(( END_TIME - START_TIME ))
+echo -e "Script execution completed successfully, $Y Time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
