a756df36414bd06103f5d78cc4170dd58cf9a286
SathivilliPraveena836 <veenasathivilli@gmail.com>
2026-05-25 11:35:27 +0530

    roboshop project

--- catalogue.sh
+++ catalogue.sh
@@ -86,6 +86,6 @@ else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+TOTAL_TIME=$($END_TIME - $START_TIME)
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
