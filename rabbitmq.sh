0ee85c8abdde7a67ff03a08138f1a680628959eb
SathivilliPraveena836 <veenasathivilli@gmail.com>
2026-05-26 21:29:33 +0530

    roboshop project

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -31,6 +31,8 @@ VALIDATE(){
         exit 1
     fi
 }
+echo "Please enter rabbitmq password to setup"
+read -s RABBITMQ_PASSWD
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 dnf install rabbitmq-server -y &>>$LOG_FILE
 VALIDATE $? "Installation of RabbitMQ"
